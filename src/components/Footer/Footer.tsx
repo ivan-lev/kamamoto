3249cdc72082aa3033ac220ba5236c44a6142359
Ivan Lev <fanatos@mail.ru>
2024-06-20 16:56:04 +0500

    feat/style: initial footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import './Footer.scss';
 
 export default function Footer() {
-  return <footer className="footer">Footer here</footer>;
+  return <footer className="footer">&copy; 2024 Сайт создан Иваном Львом</footer>;
 }
