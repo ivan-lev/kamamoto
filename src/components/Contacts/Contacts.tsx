77e314fe4f36a0a437936356b64ce6b40fe87e5c
Ivan Lev <fanatos@mail.ru>
2024-06-22 00:26:07 +0500

    feat: add initial form to Contacts component

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -3,7 +3,19 @@ import './Contacts.scss';
 export default function Contacts() {
   return (
     <section className="contacts">
-      <h2>Контакты</h2>
+      <h2 className="title title2">Контакты</h2>
+      <form className="container background-muted bordered contacts__form">
+        <input className="contacts__input background-muted bordered" placeholder="Имя"></input>
+        <input className="contacts__input background-muted bordered" placeholder="Почта"></input>
+        <textarea
+          className="contacts__input background-muted bordered contacts__textarea"
+          placeholder="Сообщение"
+          rows={5}
+        ></textarea>
+        <button className="contacts__submit" type="submit">
+          Отправить
+        </button>
+      </form>
     </section>
   );
 }
