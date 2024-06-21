d9006c826a27995c73f7e82fba22d815eb128d56
Ivan Lev <fanatos@mail.ru>
2024-06-21 18:50:40 +0500

    feat: add initial Exhibitions component

---
+++ src/components/Exhibitions/Exhibitions.tsx
@@ -0,0 +1,9 @@
+import './Exhibitions.scss';
+
+export default function Exhibitions() {
+  return (
+    <section className="exhibitions">
+      <h2 className="title title2">Выставки</h2>
+    </section>
+  );
+}
