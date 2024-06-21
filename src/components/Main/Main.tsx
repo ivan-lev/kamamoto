98b48c01885473a51cf016e2e375887ad9057d86
Ivan Lev <fanatos@mail.ru>
2024-06-21 18:52:38 +0500

    feat: add all routes

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,6 +3,9 @@ import './Main.scss';
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../HomePage/HomePage';
+import About from '../About/About';
+import Collection from '../Collection/Collection';
+import Exhibitions from '../Exhibitions/Exhibitions';
 import Contacts from '../Contacts/Contacts';
 
 import 'react-image-gallery/styles/scss/image-gallery.scss';
@@ -12,6 +15,9 @@ export default function Main() {
     <main className="content">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/exhibitions" element={<Exhibitions />} />
         <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </main>
