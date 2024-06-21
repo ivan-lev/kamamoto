757802baf894c5f67845b1e216e71320187e38d6
Ivan Lev <fanatos@mail.ru>
2024-06-21 18:50:28 +0500

    feat: add initial Collection component

---
+++ src/components/Collection/Collection.tsx
@@ -0,0 +1,9 @@
+import './Collection.scss';
+
+export default function Collection() {
+  return (
+    <section className="collection">
+      <h2 className="title title2">Коллекция</h2>
+    </section>
+  );
+}
