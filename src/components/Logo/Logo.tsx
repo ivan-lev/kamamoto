66adac722cd1c8776147c8a8e6ef1da6b1e9a7de
Ivan Lev <fanatos@mail.ru>
2024-06-21 15:10:36 +0500

    refactor: use NavLink component instead of <a> tag

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,11 @@
 import './Logo.scss';
 
+import { NavLink } from 'react-router-dom';
+
 export default function Logo(): JSX.Element {
   return (
-    <a className="logo" href="#">
+    <NavLink className="logo" to="/">
       <img className="logo__link" src="logo.png" alt="logo"></img>
-    </a>
+    </NavLink>
   );
 }
