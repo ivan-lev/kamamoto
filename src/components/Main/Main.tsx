2ddfa2873849fbae8f5837b61cab60d2b27bc007
Ivan Lev <fanatos@mail.ru>
2024-06-21 15:16:09 +0500

    feat: add routes to change Main component content

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,19 @@
 import './Main.scss';
 
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from '../HomePage/HomePage';
+import Contacts from '../Contacts/Contacts';
+
+import 'react-image-gallery/styles/scss/image-gallery.scss';
+
 export default function Main() {
-  return <main className="content">Main here</main>;
+  return (
+    <main className="content">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </main>
+  );
 }
