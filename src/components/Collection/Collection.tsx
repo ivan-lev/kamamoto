a8186d2a82d2ff5ffe76eb989b5a3d28d8284bc4
Ivan Lev <fanatos@mail.ru>
2024-06-28 00:17:29 +0500

    feat: add redux actions to set collection category to be displayed

--- src/components/Collection/Collection.tsx
+++ src/components/Collection/Collection.tsx
@@ -2,18 +2,30 @@ import './Collection.scss';
 
 import { Link } from 'react-router-dom';
 
-import { CeramicKindType } from '../../types/ceramicKindType';
+import { ExhibitsCategory } from '../../types/exhibitsCategory';
+
+import { useDispatch } from 'react-redux';
+import { setExhibitsCategory, setExhibitsList } from '../../slices/exhibitsSlice';
 
 export default function Collection() {
+  const dispatch = useDispatch();
+
   return (
     <section className="section collection">
       <h2 className="title title2">Коллекция</h2>
       <ul className="">
-        {(Object.keys(CeramicKindType) as Array<keyof typeof CeramicKindType>).map(key => {
+        {(Object.keys(ExhibitsCategory) as Array<keyof typeof ExhibitsCategory>).map(key => {
           return (
             <li key={key}>
-              <Link className="footer__column-element" to={key}>
-                {CeramicKindType[key]}
+              <Link
+                className="footer__column-element"
+                to={key}
+                onClick={() => {
+                  dispatch(setExhibitsCategory(key));
+                  dispatch(setExhibitsList(key));
+                }}
+              >
+                {ExhibitsCategory[key]}
               </Link>
             </li>
           );
