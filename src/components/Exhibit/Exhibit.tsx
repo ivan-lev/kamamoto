4e2660fa648822ed6e14bb77b9f038e36fff7c79
Ivan Lev <fanatos@mail.ru>
2024-06-28 02:06:03 +0500

    feat: set rendered exhibit depending on path

--- src/components/Exhibit/Exhibit.tsx
+++ src/components/Exhibit/Exhibit.tsx
@@ -2,8 +2,10 @@ import './Exhibit.scss';
 
 import parse, { Element } from 'html-react-parser';
 
+import { useEffect } from 'react';
+
 // Redux
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../slices';
 
 //vendor components
@@ -12,16 +14,25 @@ import 'react-image-gallery/styles/scss/image-gallery.scss';
 
 import { Images } from '../../variables/images';
 
+import { handleSetExhibit } from '../../utils/handleSetExhibit';
+
 export default function Exhibit(): JSX.Element {
   const exhibit = useSelector((state: RootState) => state.exhibits.exhibit);
 
+  const dispatch = useDispatch();
+
   const images: Images = [];
 
+  useEffect(() => {
+    if (!exhibit) {
+      handleSetExhibit(dispatch);
+    }
+  }, [exhibit]);
+
   for (let i = 1; i <= 10; i++) {
-    const imageLink = `../../../public/exhibits/${exhibit?.id}/${i === 10 ? '10' : '0' + i}.jpg`;
+    const imageLink = `/exhibits/${exhibit?.id}/${i === 10 ? '10' : '0' + i}.jpg`;
     const imageObject = { original: imageLink, thumbnail: imageLink };
     images.push(imageObject);
-    console.log(imageObject);
   }
 
   const options = {
