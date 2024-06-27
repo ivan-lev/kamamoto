8f3b3f5ee3cc7df2373f1d1851a6e6f314b62c2b
Ivan Lev <fanatos@mail.ru>
2024-06-27 14:08:19 +0500

    feat: add collection list rendering logic

--- src/components/Collection/Collection.tsx
+++ src/components/Collection/Collection.tsx
@@ -1,9 +1,24 @@
 import './Collection.scss';
 
+import { Link } from 'react-router-dom';
+
+import { CeramicKindType } from '../../types/ceramicKindType';
+
 export default function Collection() {
   return (
-    <section className="collection">
+    <section className="section collection">
       <h2 className="title title2">Коллекция</h2>
+      <ul className="">
+        {(Object.keys(CeramicKindType) as Array<keyof typeof CeramicKindType>).map(key => {
+          return (
+            <li key={key}>
+              <Link className="footer__column-element" to={key}>
+                {CeramicKindType[key]}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
