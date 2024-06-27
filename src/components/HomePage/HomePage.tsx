5eba59a7f113daf400f070444b4ef9b6a44e450c
Ivan Lev <fanatos@mail.ru>
2024-06-28 00:20:02 +0500

    refactor: import styles in component and wrap returned layout in 'section' block

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import './HomePage.scss';
 
 //vendor components
 import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/scss/image-gallery.scss';
 
 //components
 import Statistics from '../Statistics/Statistics';
@@ -11,10 +12,10 @@ import { images } from '../../variables/images';
 
 export default function HomePage() {
   return (
-    <>
+    <section className="section">
       <h1 className="title title1">Kamamoto - коллекция японской керамики</h1>
       <ImageGallery items={images} />
       <Statistics />
-    </>
+    </section>
   );
 }
