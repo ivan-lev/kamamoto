32d9f093ef469aa4f6d692aa937d1438becc6638
Ivan Lev <fanatos@mail.ru>
2024-07-03 20:58:16 +0500

    fix: update paths for catogory images

--- src/variables/categoryList.ts
+++ src/variables/categoryList.ts
@@ -6,7 +6,7 @@ const keys = Object.keys(ExhibitCategory);
 export const categoryList: displayListType[] = keys.map((key, index) => {
   return {
     id: index,
-    thumb: `../../public/images/categories/${key}.jpg`,
+    thumb: `images/categories/${key}.jpg`,
     name: ExhibitCategory[key as keyof typeof ExhibitCategory],
     link: `/collection/${key}`
   };
