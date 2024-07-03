d9b046f6b6211e7a0f9589a5fd0857b2a9a5e82c
Ivan Lev <fanatos@mail.ru>
2024-07-03 20:57:03 +0500

    feat: update info about collection owner

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -24,18 +24,34 @@ export default function About() {
       <div id="about-author" className="about__content container background-muted bordered">
         <h3 className="title title3">Созерцая красоту</h3>
         <p className="text text_muted">
-          As a freelance web designer and developer, I bring a unique combination of creativity and
-          technical expertise to every project. With a keen eye for design and a passion for
-          delivering user-friendly web experiences, I work closely with clients to understand their
-          needs and bring their vision to life.
+          Меня зовут Иван. Я коллекционирую вещи, созданные японскими мастерами.
         </p>
         <p className="text text_muted">
-          My approach is rooted in collaboration and communication, and I take pride in my ability
-          to explain technical concepts in simple terms. Whether I'm developing a new website from
-          scratch or optimizing an existing site for search engines, I always strive for excellence
-          in both form and function. With a dedication to quality and a commitment to staying on top
-          of the latest trends and technologies, I am confident in my ability to deliver exceptional
-          results that exceed my clients' expectations.
+          Культура и искусство Востока всегда привлекали меня своей глубиной и разнообразием, а
+          непосредственный контакт и погружение начался примерно в 2008 году, когда я всерьёз
+          увлёкся китайским чаем. У меня появились первые атрибуты для приготовления напитка, и я
+          стал активно изучать эту сферу. Информации в то время было мало, и приходилось искать всё
+          самому: так я научился находить и собирать знания, использовать их крупицы, чтобы сложить
+          более полную картину изучаемого.
+        </p>
+        <p className="text text_muted">
+          В результате у меня появился небольшой проект, посвященный китайскому чаю, который
+          благополучно существовал до 2015 года, когда я, изучая чай других стран, открыл для себя
+          мир японской керамики (которая имеет к истории чая непосредственное отношение). Я
+          загорелся этим видом искусства, таким многогранным и разнообразным, что не хватит и целой
+          жизни, чтобы изучить его полностью. В этот момент появился новый проект, посвященные
+          японским вещам - Камамото.
+        </p>
+        <p className="text text_muted">
+          Японское слово камамото состоит из двух иероглифов: <b>窯元</b>. Первый означает печь для
+          обжига, а второй "начало" и "происхождение". Вместе они образуют слово понятие "гончарная
+          мастерская" или "печь", подразумевая непосредственное место, где производятся изделия.
+          Также словом камамото называют мастера-гончара​.
+        </p>
+        <p className="text text_muted">
+          Я счел, что это название идеально подходит для названия коллекции, вещи для которой я
+          выбираю с особой ответственностью, трепетом и внимательностью, которые развивал с 2015
+          года.
         </p>
       </div>
       <div className="about__links">
