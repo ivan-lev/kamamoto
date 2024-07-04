ff906e2a3c3c6a0c0955c7ce618726e31f9f6c49
Ivan Lev <fanatos@mail.ru>
2024-07-04 19:08:23 +0500

    refactor: put menu links in nav container

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -6,19 +6,21 @@ import { menu } from '../../variables/menu';
 
 export default function Menu(): JSX.Element {
   return (
-    <ul className="menu">
-      {menu.map(element => {
-        return (
-          <li key={element.name}>
-            <NavLink
-              className={({ isActive }) => `menu__link ${isActive ? 'menu__link_active' : ''}`}
-              to={element.link}
-            >
-              {element.name}
-            </NavLink>
-          </li>
-        );
-      })}
-    </ul>
+    <nav className="menu">
+      <ul className="menu__list">
+        {menu.map(element => {
+          return (
+            <li key={element.name}>
+              <NavLink
+                className={({ isActive }) => `menu__link ${isActive ? 'menu__link_active' : ''}`}
+                to={element.link}
+              >
+                {element.name}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
