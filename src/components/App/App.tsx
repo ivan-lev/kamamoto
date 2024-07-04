2604c5e95fbdef414c7826498fb408ad7d9af190
Ivan Lev <fanatos@mail.ru>
2024-07-04 11:42:24 +0500

    refactor: move routes to App from Main

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,38 @@
 import './App.scss';
 
+import { Routes, Route } from 'react-router-dom';
+
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+
+import HomePage from '../HomePage/HomePage';
+import About from '../About/About';
+import Collection from '../Collection/Collection';
+import Category from '../Category/Category';
+import Exhibit from '../Exhibit/Exhibit';
+import Exhibitions from '../Exhibitions/Exhibitions';
+import Contacts from '../Contacts/Contacts';
+import Files from '../Files/Files';
+import Documents from '../Documents/Documents';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<About />} />
+          <Route path="collection" element={<Collection />} />
+          <Route path="collection/:category" element={<Category />} />
+          <Route path="collection/:category/:exhibit" element={<Exhibit />} />
+          <Route path="exhibitions" element={<Exhibitions />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="files" element={<Files />} />
+          <Route path="documents" element={<Documents />} />
+        </Route>
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,31 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import './Main.scss';
 
-import { Routes, Route } from 'react-router-dom';
-
-import HomePage from '../HomePage/HomePage';
-import About from '../About/About';
-import Collection from '../Collection/Collection';
-import Category from '../Category/Category';
-import Exhibit from '../Exhibit/Exhibit';
-import Exhibitions from '../Exhibitions/Exhibitions';
-import Contacts from '../Contacts/Contacts';
-import Files from '../Files/Files';
-import Documents from '../Documents/Documents';
-
+// Outlet component render the children components
 export default function Main() {
   return (
     <main className="content">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/collection/:category" element={<Category />} />
-        <Route path="/collection/:category/:exhibit" element={<Exhibit />} />
-        <Route path="/exhibitions" element={<Exhibitions />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/files" element={<Files />} />
-        <Route path="/documents" element={<Documents />} />
-      </Routes>
+      <Outlet />
     </main>
   );
 }
