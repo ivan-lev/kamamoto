0182eeb8320d6cc32515620080003cefe2f515b6
Ivan Lev <fanatos@mail.ru>
2024-07-09 00:34:10 +0500

    refactor: change class names to proper

--- src/components/ExhibitionCard/ExhibitionCard.tsx
+++ src/components/ExhibitionCard/ExhibitionCard.tsx
@@ -9,24 +9,24 @@ export default function ExhibitionCard({ exhibition }: { exhibition: Exhibition 
   const { id, name, year, dates, city, place, link, isPageActive } = exhibition;
   return (
     <>
-      <div className="exhibition__upper-line"></div>
-      <div className="exhibition__year">{year}</div>
-      <div className="exhibition__lower-line"></div>
-      <div className="container background-muted bordered exhibition__card">
-        <p className="background-muted bordered exhibition__dates">{dates}</p>
-        <p className="exhibition__city">{city}</p>
-        <div>
+      <div className="exhibition-card__upper-line"></div>
+      <div className="exhibition-card__year">{year}</div>
+      <div className="exhibition-card__lower-line"></div>
+      <div className="container background-muted bordered exhibition-card__card">
+        <p className="background-muted bordered exhibition-card__dates">{dates}</p>
+        <p className="exhibition-card__city">{city}</p>
+        <div className="exhibition-card__main-content">
           {link ? (
-            <a className="exhibition__link" href={link} target="_blank">
+            <a className="exhibition-card__link" href={link} target="_blank">
               {name}
             </a>
           ) : (
-            <p className="exhibition__name">«{name}»</p>
+            <p className="exhibition-card__name">«{name}»</p>
           )}
-          <p className="exhibition__place">{place}</p>
+          <p className="exhibition-card__place">{place}</p>
         </div>
         {isPageActive && (
-          <Link className="link muted exhibition__more-link" to={id.toString()}>
+          <Link className="link muted exhibition-card__more-link" to={id.toString()}>
             Подробнее
           </Link>
         )}
