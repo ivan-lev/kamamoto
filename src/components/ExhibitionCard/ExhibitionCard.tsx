9499a32322934266aba43e959261a149a4536ecd
Ivan Lev <fanatos@mail.ru>
2024-07-08 16:46:58 +0500

    feat/refactor: destruct exhibition object to make code readable and render "More" page only if isPageActive field is true

--- src/components/ExhibitionCard/ExhibitionCard.tsx
+++ src/components/ExhibitionCard/ExhibitionCard.tsx
@@ -6,27 +6,30 @@ import { Link } from 'react-router-dom';
 import type { Exhibition } from '../../types/exhibitionType';
 
 export default function ExhibitionCard({ exhibition }: { exhibition: Exhibition }): JSX.Element {
+  const { id, name, year, dates, city, place, link, isPageActive } = exhibition;
   return (
     <>
       <div className="exhibition__upper-line"></div>
-      <div className="exhibition__year">{exhibition.year}</div>
+      <div className="exhibition__year">{year}</div>
       <div className="exhibition__lower-line"></div>
       <div className="container background-muted bordered exhibition__card">
-        <p className="background-muted bordered exhibition__dates">{exhibition.dates}</p>
-        <p className="exhibition__city">{exhibition.city}</p>
+        <p className="background-muted bordered exhibition__dates">{dates}</p>
+        <p className="exhibition__city">{city}</p>
         <div>
-          {exhibition.link ? (
-            <a className="exhibition__link" href={exhibition.link} target="_blank">
-              {exhibition.name}
+          {link ? (
+            <a className="exhibition__link" href={link} target="_blank">
+              {name}
             </a>
           ) : (
-            <p className="exhibition__name">«{exhibition.name}»</p>
+            <p className="exhibition__name">«{name}»</p>
           )}
-          <p className="exhibition__place">{exhibition.place}</p>
+          <p className="exhibition__place">{place}</p>
         </div>
-        <Link className="link muted exhibition__more-link" to={exhibition.id.toString()}>
-          Подробнее
-        </Link>
+        {isPageActive && (
+          <Link className="link muted exhibition__more-link" to={id.toString()}>
+            Подробнее
+          </Link>
+        )}
       </div>
     </>
   );
