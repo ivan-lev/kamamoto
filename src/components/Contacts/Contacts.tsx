cbe28b2f997240b4ce2e9553d410b22bfb06050e
Ivan Lev <fanatos@mail.ru>
2024-07-09 14:00:24 +0500

    style: add warning temp text while ts logic is not implemented

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -4,6 +4,10 @@ export default function Contacts() {
   return (
     <section className="section contacts">
       <h2 className="title title2">Контакты</h2>
+      <p className="text muted">
+        Данный раздел находится в стадии разработки, для связи прошу использовать ссылки в нижней
+        части сайта.
+      </p>
       <form className="container background-muted bordered contacts__form">
         <input className="contacts__input background-muted bordered" placeholder="Имя"></input>
         <input className="contacts__input background-muted bordered" placeholder="Почта"></input>
