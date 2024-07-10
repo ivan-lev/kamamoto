faa436de9041fdbd9b0083ab0b19e365766c0637
Ivan Lev <fanatos@mail.ru>
2024-07-11 01:05:03 +0500

    refactor: add 'additional' path for additional photos rendering

--- src/utils/generateImageLinks.ts
+++ src/utils/generateImageLinks.ts
@@ -1,10 +1,15 @@
 import { Images } from '../types/imageType';
 
-export const generateImageLinks = (path: string, id: number, photosCount: number = 10): Images => {
+export const generateImageLinks = (
+  path: string,
+  id: number,
+  photosCount: number = 10,
+  additional: boolean = false
+): Images => {
   let images: Images = [];
 
   for (let i = 0; i < photosCount; i++) {
-    const link = `${path}${id}/${i}.jpg`;
+    const link = `${path}${id}/${additional ? 'additional/' : ''}${i}.jpg`;
     images.push({ original: link, thumbnail: link });
   }
   return images;
