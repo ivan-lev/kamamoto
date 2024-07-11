fed82388155034b65b12a702e0470c5a7e13a2db
Ivan Lev <fanatos@mail.ru>
2024-07-12 01:42:57 +0500

    feat: add exhibit complectation enum

--- src/types/exhibitComplectationType.ts
+++ src/types/exhibitComplectationType.ts
@@ -1,13 +1,23 @@
 export enum ExhibitComplectation {
   bowl = 'чаша',
+  chaire = 'керамическая чайница',
+  chashaku = 'бамбуковаля лопаточка',
   cup = 'пиала',
   cups2 = 'две пиалы',
   cups3 = 'три пиалы',
+  kama = 'чугунный котёл с крышкой',
+  kamakan = 'кольца для переноса котла',
+  kogo = 'контейнер из двух частей',
+  natsume = 'чайница лакированная',
   plate = 'тарелка',
-  tomobakoWooden = 'деревянная коробка',
-  tomobakoCarton = 'картонная коробка',
   protectiveCloth = 'защитная ткань',
   protectiveCloths2 = 'две защитных ткани',
   paperDocument = 'бумажный документ',
-  paperDocuments2 = 'два бумажных документа'
+  paperDocuments2 = 'два бумажных документа',
+  shifuku = 'мешочек сифуку',
+  teapot = 'чайник с крышкой',
+  tomobakoBamboo = 'бамбуковая коробка',
+  tomobakoCarton = 'картонная коробка',
+  tomobakoWooden = 'деревянная коробка',
+  vase = 'ваза'
 }
