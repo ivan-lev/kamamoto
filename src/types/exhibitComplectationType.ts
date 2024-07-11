337b108f5fd947c5c2caf6c166a73becab8d53d2
Ivan Lev <fanatos@mail.ru>
2024-07-11 14:55:48 +0500

    feat: add complectation type

---
+++ src/types/exhibitComplectationType.ts
@@ -0,0 +1,13 @@
+export enum ExhibitComplectation {
+  bowl = 'чаша',
+  cup = 'пиала',
+  cups2 = 'две пиалы',
+  cups3 = 'три пиалы',
+  plate = 'тарелка',
+  tomobakoWooden = 'деревянная коробка',
+  tomobakoCarton = 'картонная коробка',
+  protectiveCloth = 'защитная ткань',
+  protectiveCloths2 = 'две защитных ткани',
+  paperDocument = 'бумажный документ',
+  paperDocuments2 = 'два бумажных документа'
+}
