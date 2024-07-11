0f8f986084d0ee48d4a55c5572cf1e81383994b3
Ivan Lev <fanatos@mail.ru>
2024-07-11 14:57:14 +0500

    feat: add new fields to exhibit type

--- src/types/exhibitType.ts
+++ src/types/exhibitType.ts
@@ -1,11 +1,14 @@
-import { ExhibitCategory } from './exhibitCategory';
-import { CeramicStyleType } from './ceramicStyleType';
+import type { ExhibitCategory } from './exhibitCategory';
+import type { CeramicStyleType } from './ceramicStyleType';
+import type { ExhibitComplectation } from './exhibitComplectationType';
 
 export type Exhibit = {
   id: number;
   name: string;
+  age?: string;
   potterName?: string;
   potterJapaneseName?: string;
+  potterLifeDates?: string;
   potterPhoto?: string;
   additionalPhotos: boolean;
   additionalPhotosCount?: number;
@@ -14,10 +17,13 @@ export type Exhibit = {
   description: string;
   additionalDescription?: string;
   price: number;
-  weigth?: number;
   height?: number;
   length?: number;
   width?: number;
+  weigth?: number;
+  weightOfSet?: number;
+  complectation: ExhibitComplectation[];
+  preservation?: string;
 };
 
 export type Exhibits = Exhibit[];
