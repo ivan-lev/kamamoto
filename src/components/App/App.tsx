e67965af2fcf9bc3b14066bcadab688906c4a3c4
Ivan Lev <fanatos@mail.ru>
2024-07-18 00:08:15 +0500

    refactor: move Helmet provider to main.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.scss';
 // React
 import { Routes, Route } from 'react-router-dom';
 
-import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { Helmet } from 'react-helmet-async';
 
 // Variables
 import { documents } from '../../variables/documents';
@@ -27,36 +27,31 @@ import NotFound from '../NotFound/NotFound';
 export default function App() {
   return (
     <>
-      <HelmetProvider>
-        <Helmet>
-          <title>{`Камамото - японская керамика`}</title>
-          <meta property="og:title" content={`Камамото - японская керамика`} />
-          <meta property="og:image" content={`https://kamamoto.ru/images/og-image.jpg`} />
-        </Helmet>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route index element={<HomePage />} />
-            <Route path="about/" element={<About />} />
-            <Route path="collection/" element={<Collection />} />
-            <Route path="collection/:category/" element={<Category />} />
-            <Route path="collection/:category/:exhibit" element={<Exhibit />} />
-            <Route path="expos/" element={<Expos />} />
-            <Route path="expos/:exhibition" element={<Exhibition />} />
-            <Route path="contacts/" element={<Contacts />} />
-            <Route
-              path="downloads/"
-              element={<Files title="Файлы для скачивания" files={files} />}
-            />
-            <Route
-              path="documents/"
-              element={<Files title="Шаблоны документов" files={documents} />}
-            />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </HelmetProvider>
+      <Helmet>
+        <title>{`Камамото - японская керамика`}</title>
+        <meta property="og:title" content={`Камамото - японская керамика`} />
+        <meta property="og:image" content={`https://kamamoto.ru/images/og-image.jpg`} />
+      </Helmet>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<HomePage />} />
+          <Route path="about/" element={<About />} />
+          <Route path="collection/" element={<Collection />} />
+          <Route path="collection/:category/" element={<Category />} />
+          <Route path="collection/:category/:exhibit" element={<Exhibit />} />
+          <Route path="expos/" element={<Expos />} />
+          <Route path="expos/:exhibition" element={<Exhibition />} />
+          <Route path="contacts/" element={<Contacts />} />
+          <Route path="downloads/" element={<Files title="Файлы для скачивания" files={files} />} />
+          <Route
+            path="documents/"
+            element={<Files title="Шаблоны документов" files={documents} />}
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './slices/index.ts';
 
+import { HelmetProvider } from 'react-helmet-async';
+
 import App from './components/App/App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
     </Provider>
   </React.StrictMode>
 );
