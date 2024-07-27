eeb34cb7e780f35c087b6c67aed119f6bb7ea0d4
Ivan Lev <fanatos@mail.ru>
2024-07-27 14:53:08 +0500

    feat: add variable with data rendered in ThanksLetters compponent

---
+++ src/variables/thanksLetters.ts
@@ -0,0 +1,11 @@
+import type { Resources } from '../types/fileType';
+
+export const thanksLetters: Resources = [
+  {
+    id: 0,
+    preview: '/public/thanks-letters/2024-ume-preview.jpg',
+    link: '/thanks-letters/2024-ume.pdf',
+    description:
+      'Благодарственное письмо от Ю.В. Поповой, директора центра Восточных языков и культуры "Юмэ"'
+  }
+];
