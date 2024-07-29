98d9679c76dd2c640757145f3606bf206b64c6e5
Ivan Lev <fanatos@mail.ru>
2024-07-29 10:16:35 +0500

    feat: add preloader while pictures loading

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,10 @@
 import './Card.scss';
 
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
+
+import preloader from '/public/icons/preloader.svg';
 
 export default function Card({
   link,
@@ -11,11 +15,22 @@ export default function Card({
   name: string;
   image: string;
 }): JSX.Element {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div className="card">
-      <Link className="link" to={link}>
-        <img className="card__image" src={image}></img>
-      </Link>
+      <div className="card__image-wrapper">
+        {loading && <img className="card__preloader" alt="Exhibit preview" src={preloader}></img>}
+        <Link className="link" to={link} style={{ display: loading ? 'none' : 'flex' }}>
+          <img
+            className="card__image"
+            alt="Exhibit preview"
+            src={image}
+            onLoad={() => setLoading(false)}
+          ></img>
+        </Link>
+      </div>
+
       <Link className="link card__link" to={link}>
         <span className="card__name">{name}</span>
       </Link>
