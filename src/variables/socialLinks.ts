14976b6c6dc7e26d5e88da17566b30b5e139804b
Ivan Lev <fanatos@mail.ru>
2024-07-30 09:16:15 +0500

    feat: add personal social links

--- src/variables/socialLinks.ts
+++ src/variables/socialLinks.ts
@@ -3,6 +3,33 @@ import { SocialLinks } from '../types/socialLinkType';
 export const socialLinks: SocialLinks = [
   {
     id: 0,
+    title: 'Instagram',
+    link: 'https://instagram.com/chawan.shop',
+    icon: '/icons/logo-instagram.svg'
+  },
+  {
+    id: 1,
+    title: 'Telegram',
+    link: 'https://t.me/ChawanShop',
+    icon: '/icons/logo-telegram.svg'
+  },
+  {
+    id: 2,
+    title: 'Email',
+    link: 'mailto:fanatos@mail.ru',
+    icon: '/icons/logo-mail.svg'
+  },
+  {
+    id: 3,
+    title: 'Созидатели',
+    link: 'https://www.sozidateli.ru/profile/303854',
+    icon: '/icons/logo-sozidateli.svg'
+  }
+];
+
+export const personalSocialLinks: SocialLinks = [
+  {
+    id: 0,
     title: 'VK',
     link: 'https://vk.com/tea_lion',
     icon: '/icons/logo-vk.svg'
@@ -24,11 +51,5 @@ export const socialLinks: SocialLinks = [
     title: 'Email',
     link: 'mailto:fanatos@mail.ru',
     icon: '/icons/logo-mail.svg'
-  },
-  {
-    id: 4,
-    title: 'Созидатели',
-    link: 'https://www.sozidateli.ru/profile/303854',
-    icon: '/icons/logo-sozidateli.svg'
   }
 ];
