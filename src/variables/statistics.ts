8f830a3475fea1e0c48dc7b425356693135d58cf
Ivan Lev <fanatos@mail.ru>
2024-07-30 12:00:53 +0500

    style: update statistics

--- src/variables/statistics.ts
+++ src/variables/statistics.ts
@@ -2,6 +2,6 @@ import type { StatisticsType } from '../types/statisticsType';
 
 export const statistics: StatisticsType = [
   { header: 'Начало', value: '2015' },
-  { header: 'Выставки', value: '5' },
+  { header: 'Выставки', value: '6' },
   { header: 'Экспонаты', value: '200+' }
 ];
