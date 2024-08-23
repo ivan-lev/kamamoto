5f1e326ba77842f584558a9508e1e2e15b366d95
Ivan Lev <fanatos@mail.ru>
2024-08-23 22:55:17 +0500

    feat: add initial Partners component

---
+++ src/components/Partners/Partners.tsx
@@ -0,0 +1,9 @@
+import './Partners.scss';
+
+export default function Partners(): JSX.Element {
+  return (
+    <section className="section">
+      <div>Компонент, в котором будут логотипы организаций партнеров</div>
+    </section>
+  );
+}
