840a15b7277e367a2bbd81ad0d3f7ed03af3dea8
Ivan Lev <fanatos@mail.ru>
2024-08-23 22:56:58 +0500

    feat: add techniques type

---
+++ src/types/techniques.ts
@@ -0,0 +1,9 @@
+enum techniques {
+  clay = 'глина',
+  enamels = 'эмали',
+  engraving = 'гравировка',
+  firing = 'обжиг',
+  glaze = 'глазурь',
+  painting = 'роспись',
+  stamping = 'тиснение'
+}
