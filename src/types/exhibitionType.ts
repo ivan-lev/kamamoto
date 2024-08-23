6662f83f353b8f2b0fb0d93a421e2e17b2c20d1b
Ivan Lev <fanatos@mail.ru>
2024-08-24 02:34:53 +0500

    refactor: make address and poster fileds required in exhibition type

--- src/types/exhibitionType.ts
+++ src/types/exhibitionType.ts
@@ -4,15 +4,15 @@ export type Exhibition = {
   dates: string;
   city: string;
   place: string;
-  address?: string;
+  address: string;
   name: string;
   link?: string;
-  description?: string;
+  description: string;
   photosCount?: number;
-  poster?: boolean;
+  poster: boolean;
   curators?: string;
   organisators?: string;
-  isPageActive: boolean;
+  isActive: boolean;
 };
 
 export type Exhibitions = Exhibition[];
