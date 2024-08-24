a605b80ee45ec53d34dffa7ad8a2d74122121e0f
Ivan Lev <fanatos@mail.ru>
2024-08-25 00:32:32 +0500

    fix: update variable name

--- src/components/ExhibitionCard/ExhibitionCard.tsx
+++ src/components/ExhibitionCard/ExhibitionCard.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import type { Exhibition } from '../../types/exhibitionType';
 
 export default function ExhibitionCard({ exhibition }: { exhibition: Exhibition }): JSX.Element {
-  const { id, name, year, dates, city, place, isPageActive } = exhibition;
+  const { id, name, year, dates, city, place, isActive } = exhibition;
   return (
     <>
       <div className="exhibition-card__upper-line"></div>
@@ -19,7 +19,7 @@ export default function ExhibitionCard({ exhibition }: { exhibition: Exhibition 
           <p className="exhibition-card__name">{name}</p>
           <p className="exhibition-card__place">{place}</p>
         </div>
-        {isPageActive && (
+        {isActive && (
           <Link className="link muted exhibition-card__more-link" to={id.toString()}>
             Подробнее
           </Link>
