a0f0c007e4350d78f10ae28b0f928b118031f515
Ivan Lev <fanatos@mail.ru>
2024-08-24 23:57:58 +0500

    refactor/style: add initial exhibit add form and it's styles

--- src/components/AdminExbitions/AdminExhibitions.tsx
+++ src/components/AdminExbitions/AdminExhibitions.tsx
@@ -9,7 +9,7 @@ import { api } from '../../utils/api';
 export default function AdminExhibitions(): JSX.Element {
   const emptyExhibition: Exhibition = {
     id: 0,
-    year: 0,
+    year: 2024,
     dates: '',
     city: '',
     address: '',
@@ -45,10 +45,13 @@ export default function AdminExhibitions(): JSX.Element {
   } = exhibitionToCreate;
 
   useEffect(() => {
-    api.getExhibitions().then(exhibitions => setExhibitions(exhibitions));
+    api.getExhibitions().then(exhibitions => {
+      setExhibitions(exhibitions);
+      setExhibitionToCreate({ ...exhibitionToCreate, id: exhibitions.length + 1 });
+    });
   }, []);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setExhibitionToCreate({ ...exhibitionToCreate, [name]: value });
   };
@@ -84,97 +87,205 @@ export default function AdminExhibitions(): JSX.Element {
   };
 
   return (
-    <div>
+    <div className="admin-exhibition">
       <div className="admin-exhibition__list">
-        <span>ID</span>
-        <span>Название</span>
-        <span>Город</span>
-        <span>Год</span>
-        <span>Акт-сть</span>
+        <div className="admin-exhibition__row">
+          <span>ID</span>
+          <span>Название</span>
+          <span>Город</span>
+          <span>Год</span>
+          <span>Акт-сть</span>
+        </div>
+        {exhibitions.map(exhibition => {
+          return (
+            <div key={exhibition.id} className="admin-exhibition__row">
+              <span>{exhibition.id}</span>
+              <span>{exhibition.name}</span>
+              <span>{exhibition.city}</span>
+              <span>{exhibition.year}</span>
+              <span>{exhibition.isActive ? 'Да' : 'Нет'}</span>
+            </div>
+          );
+        })}
       </div>
-      {exhibitions.map(exhibition => {
-        return (
-          <div className="admin-exhibition__list">
-            <span>{exhibition.id}</span>
-            <span>{exhibition.name}</span>
-            <span>{exhibition.city}</span>
-            <span>{exhibition.year}</span>
-            <span>{exhibition.isActive ? 'Да' : 'Нет'}</span>
+      <form
+        className="background-muted bordered admin-exhibition__form"
+        onSubmit={handleCreateExhibition}
+      >
+        <fieldset className="admin-exhibition__fieldset">
+          <legend className="admin-exhibition__field-legend">Добавить выставку</legend>
+
+          <div className="admin-exhibition__fields-row">
+            <div className="admin-exhibition__field admin-exhibition__field-id">
+              <span>id: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="id"
+                placeholder="id"
+                value={id}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-year">
+              <span>год: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="year"
+                placeholder="год"
+                value={year}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-date">
+              <span>даты: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="dates"
+                placeholder="даты"
+                value={dates}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-        );
-      })}
-      <form onSubmit={handleCreateExhibition}>
-        <span>id: </span>
-        <input type="text" name="id" placeholder="id" value={id} onChange={handleChange} />
-        <span>год: </span>
-        <input type="text" name="year" placeholder="год" value={year} onChange={handleChange} />
-        <span>дата: </span>
-        <input type="text" name="dates" placeholder="дата" value={dates} onChange={handleChange} />
-        <span>город: </span>
-        <input type="text" name="city" placeholder="город" value={city} onChange={handleChange} />
-        <span>адрес: </span>
-        <input
-          type="text"
-          name="address"
-          placeholder="адрес"
-          value={address}
-          onChange={handleChange}
-        />
-        <span>место: </span>
-        <input
-          type="text"
-          name="place"
-          placeholder="название галереи"
-          value={place}
-          onChange={handleChange}
-        />
-        <span>название: </span>
-        <input
-          type="text"
-          name="name"
-          placeholder="название выставки"
-          value={name}
-          onChange={handleChange}
-        />
-        <span>ссылка: </span>
-        <input type="text" name="link" placeholder="ссылка" value={link} onChange={handleChange} />
-        <span>описание: </span>
-        <input
-          type="text"
-          name="description"
-          placeholder="описание"
-          value={description}
-          onChange={handleChange}
-        />
-        <span>кол-во фото: </span>
-        <input
-          type="text"
-          name="photosCount"
-          placeholder="кол-во фотографий"
-          value={photosCount}
-          onChange={handleChange}
-        />
-        <span>есть постер: </span>
-        <input type="checkbox" name="poster" placeholder="постеры" onChange={handleCheckBox} />
-        <span>кураторы: </span>
-        <input
-          type="text"
-          name="curators"
-          placeholder="кураторы"
-          value={curators}
-          onChange={handleChange}
-        />
-        <span>организаторы: </span>
-        <input
-          type="text"
-          name="organisators"
-          placeholder="организаторы"
-          value={organisators}
-          onChange={handleChange}
-        />
-        <span>активность: </span>
-        <input type="checkbox" name="isActive" placeholder="активность" onChange={handleCheckBox} />
-        <button type="submit">Отправить</button>
+
+          <div className="admin-exhibition__fields-row">
+            <div className="admin-exhibition__field admin-exhibition__field-name">
+              <span>название: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="name"
+                placeholder="название выставки"
+                value={name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-city">
+              <span>город: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="city"
+                placeholder="город"
+                value={city}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="admin-exhibition__fields-row">
+            <div className="admin-exhibition__field admin-exhibition__field-address">
+              <span>адрес: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="address"
+                placeholder="адрес"
+                value={address}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-place">
+              <span>место проведения: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="place"
+                placeholder="место проведения"
+                value={place}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="admin-exhibition__fields-row">
+            <div className="admin-exhibition__field admin-exhibition__field-curators">
+              <span>кураторы: </span>
+              <textarea
+                className="background-muted bordered textarea input"
+                name="curators"
+                placeholder="кураторы"
+                value={curators}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-organisators">
+              <span>организаторы: </span>
+              <textarea
+                className="background-muted bordered textarea input"
+                name="organisators"
+                placeholder="организаторы"
+                value={organisators}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="admin-exhibition__fields-row">
+            <div className="admin-exhibition__field admin-exhibition__field-description">
+              <span>описание: </span>
+              <textarea
+                className="background-muted bordered textarea input"
+                name="description"
+                placeholder="описание"
+                value={description}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="admin-exhibition__fields-row">
+            <div className="admin-exhibition__field admin-exhibition__field-link">
+              <span>ссылка: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="link"
+                placeholder="ссылка"
+                value={link}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-photos-count">
+              <span>кол-во фото: </span>
+              <input
+                className="background-muted bordered input"
+                type="text"
+                name="photosCount"
+                placeholder="кол-во фотографий"
+                value={photosCount}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-poster">
+              <span>постер: </span>
+              <input
+                className="background-muted bordered input"
+                type="checkbox"
+                name="poster"
+                onChange={handleCheckBox}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-is-active">
+              <span>активность: </span>
+              <input
+                className="background-muted bordered input"
+                type="checkbox"
+                name="isActive"
+                placeholder="активность"
+                onChange={handleCheckBox}
+              />
+            </div>
+            <div className="admin-exhibition__field admin-exhibition__field-submit">
+              <button className="admin-exhibition__field-button button" type="submit">
+                Отправить
+              </button>
+            </div>
+          </div>
+        </fieldset>
       </form>
     </div>
   );
