280e39a68af98c13c80b9fa24d29cefb78070ca4
Ivan Lev <fanatos@mail.ru>
2024-08-26 09:52:12 +0500

    feat: add initial statistics logic

--- src/components/AdminStatistics/AdminStatistics.tsx
+++ src/components/AdminStatistics/AdminStatistics.tsx
@@ -1,5 +1,50 @@
 import './AdminStatistics.scss';
 
+// React
+import { useEffect, useState } from 'react';
+
+// Utils
+import { api } from '../../utils/api';
+
+type Statistics = {
+  exhibits: number;
+  exhibitions: number;
+};
+
+const defaultStatistics = { exhibits: 0, exhibitions: 0 };
+
 export default function AdminStatistics(): JSX.Element {
-  return <div>Статистика</div>;
+  const [statistics, setStatistics] = useState<Statistics>(defaultStatistics);
+  useEffect(() => {
+    api
+      .getStatistics()
+      .then(response => setStatistics(response))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className="admin-statistics container">
+      <h2 className="title3">Статистика</h2>
+      <div className="admin-statistics__grid">
+        <div className="bordered background-muted admin-statistics__element">
+          <img
+            className="admin-statistics__element-icon"
+            src="/public/icons/exhibitions.svg"
+            alt="иконка"
+          />
+          <span className="admin-statistics__element-title">
+            Выставки: {statistics.exhibitions}
+          </span>
+        </div>
+        <div className="bordered background-muted admin-statistics__element">
+          <img
+            className="admin-statistics__element-icon"
+            src="/public/icons/exhibits.svg"
+            alt="иконка"
+          />
+          <span className="admin-statistics__element-title">Экспонаты: {statistics.exhibits}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
