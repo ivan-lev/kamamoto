97edebdc8438a9245ca16f4de21db210521c1dfb
Ivan Lev <fanatos@mail.ru>
2024-08-27 21:25:29 +0500

    feat: add login error messages

--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -1,5 +1,11 @@
 export const PATHS = {
-  BASE_URL: 'http://localhost:5173/',
-  EXHIBIT_PATH: '/exhibits/',
+  BASE_URL: 'http://localhost:5173',
+  EXHIBIT_PATH: 'exhibits',
   EXHIBITION_PATH: 'exhibitions'
+};
+
+export const LOGIN_MESSAGES = {
+  WRONG_CREDEINTIALS: 'Неправильный логин или пароль.',
+  LOGIN_ERROR: 'Ошибка логина...',
+  TOKEN_ERROR: 'Ошибка проверки токена.'
 };
