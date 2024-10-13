f97f2f63a7bfe3ba32c9023fc11ff41a6d1c6154
Ivan Lev <fanatos@mail.ru>
2024-10-13 15:38:14 +0500

    refactor: style util regarding to antfy eslint rules

--- src/utils/generateImageLinks.ts
+++ src/utils/generateImageLinks.ts
@@ -1,16 +1,12 @@
-import { Images } from '../types/imageType';
+import type { Images } from '../types/imageType';
 
-export const generateImageLinks = (
-  path: string,
-  photos: string[],
-  additional: boolean = false
-): Images => {
-  let images: Images = [];
+export function generateImageLinks(path: string, photos: string[], additional: boolean = false): Images {
+  const images: Images = [];
 
-  photos.forEach(photo => {
+  photos.forEach((photo) => {
     const link = `${path}/${additional ? 'additional/' : ''}${photo}`;
     images.push({ original: link, thumbnail: link });
   });
 
   return images;
-};
+}
