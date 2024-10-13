06c22a63fa1d67b6859a8986afadb44fa52ca1f1
Ivan Lev <fanatos@mail.ru>
2024-10-13 14:29:18 +0500

    feat: add new variables

--- src/variables/variables.ts
+++ src/variables/variables.ts
@@ -4,16 +4,17 @@ export const PATHS = {
   EXHIBITS: 'exhibits',
   EXHIBITIONS: 'exhibitions',
   IMAGES: 'images',
+  LETTERS: 'letters',
   PARTNERS: 'partners',
   RESOURSES: 'http://localhost:5173',
   SIGNIN: 'signin',
   STATISTICS: 'statistics',
-  USERS: 'users'
+  USERS: 'users',
 };
 
 export const LOGIN_MESSAGES = {
   WRONG_CREDEINTIALS: 'Неправильный email или пароль.',
   LOGIN_ERROR: 'Ошибка логина...',
   TOKEN_ERROR: 'Ошибка проверки токена.',
-  WRONG_EMAIL_FORMAT: 'Неправильный фармат email-адреса'
+  WRONG_EMAIL_FORMAT: 'Неправильный фармат email-адреса',
 };
