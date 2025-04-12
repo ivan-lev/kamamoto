730c82f6cf46ca657041d62a36715e91dbf0f326
Ivan Lev <fanatos@mail.ru>
2025-04-12 20:48:55 +0500

    refactor: rename filename

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,8 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
-import './styles/index.scss';
+import '@/styles/_index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
