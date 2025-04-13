b50d41154b5d23c7ac8e109b63e4c874fa0f0770
Ivan Lev <fanatos@mail.ru>
2025-04-13 12:55:28 +0500

    refactor: update modal component

--- front/src/components/Modal/Modal.tsx
+++ front/src/components/Modal/Modal.tsx
@@ -19,24 +19,22 @@ export default function Modal({ showModal, closeModal, content }: ModalInterface
 	}
 
 	useEffect(() => {
-		if (showModal ) {
-			handleOpenModal();
-		}
-
-		if (!showModal) {
-			handleCloseModal();
-		}
+		showModal ? handleOpenModal() : handleCloseModal();
 	}, [showModal]);
 
 	return (
-		showModal &&
-		<>
-		<div className="modal" ref={dialogRef}>
-			<div className='modal__backdrop' onClick={handleCloseModal}></div>
-			<div className='modal__content'>
-				{ content }
-			</div>
-			<button onClick={handleCloseModal}>Спрятать диалог</button>
-		</div></>
+		showModal
+		&& (
+			<>
+				<div className="modal" ref={dialogRef}>
+					<div className="modal__backdrop" onClick={handleCloseModal}></div>
+					<div className="modal__content">
+						{ content }
+
+						<button className="button" onClick={handleCloseModal}>Закрыть окно</button>
+					</div>
+				</div>
+			</>
+		)
 	);
 }
