e49abad9b1106ed506e0f233c344aeec4f758de6
Ivan Lev <fanatos@mail.ru>
2025-05-09 19:23:01 +0500

    feat: remove unexpected consolelogs from builds

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,6 +5,14 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+			},
+		},
+	},
 	plugins: [react()],
 	css: {
 		preprocessorOptions: {
