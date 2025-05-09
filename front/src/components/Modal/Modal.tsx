3d06cc875e6281ffb729ea2ff9f19e1ddd4a91a4
Ivan Lev <fanatos@mail.ru>
2025-05-09 21:08:52 +0500

    feat: improve Modal component

--- front/src/components/Modal/Modal.tsx
+++ front/src/components/Modal/Modal.tsx
@@ -3,58 +3,55 @@ import { motionSettings } from '@/variables/motion';
 import { AnimatePresence, motion } from 'motion/react';
 import { useEffect } from 'react';
 
-const { states, transition } = motionSettings;
+const { variants, transition } = motionSettings;
 
-interface ModalInterface {
+interface Props {
 	showModal: boolean;
-	closeModal: any;
+	closeModal: () => void;
 	children: ReactNode;
 }
 
-export default function Modal({ showModal, closeModal, children }: ModalInterface) {
+export default function Modal({ showModal, closeModal, children }: Props) {
+	function handleOpenModal() {
+		document.body.style.overflow = 'hidden';
+		document.addEventListener('keydown', closeModalOnEsc, false);
+	}
+
 	function handleCloseModal() {
 		document.body.style.overflow = 'unset';
 		closeModal();
+		document.removeEventListener('keydown', closeModalOnEsc, false);
 	}
 
-	function handleCloseModalOnEsc(event: KeyboardEvent) {
+	function closeModalOnEsc(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
 			handleCloseModal();
 		}
 	}
 
-	function handleOpenModal() {
-		document.body.style.overflow = 'hidden';
-	}
-
 	useEffect(() => {
 		showModal ? handleOpenModal() : handleCloseModal();
-		showModal
-			? document.addEventListener('keydown', handleCloseModalOnEsc, false)
-			: document.removeEventListener('keydown', handleCloseModalOnEsc, false);
 	}, [showModal]);
 
 	return (
 		<AnimatePresence>
 			{ showModal && (
 				<motion.div
-					initial={states.hidden}
-					animate={states.visible}
-					exit={states.hidden}
+					initial={variants.hidden}
+					animate={variants.visible}
+					exit={variants.hidden}
 					transition={transition}
 					key="modal"
 					className="modal"
 				>
 					<div className="modal__backdrop" onClick={handleCloseModal}></div>
+
 					<div className="modal__content">
 						{ children }
-
 						<button className="button" onClick={handleCloseModal}>Закрыть окно</button>
 					</div>
-
 				</motion.div>
 			)}
-
 		</AnimatePresence>
 	);
 }
