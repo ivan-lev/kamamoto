59084766e8ca21aa397bd9b944e321e0a38c70ac
Ivan Lev <fanatos@mail.ru>
2025-08-30 02:34:57 +0500

    feat(SliderMain): add new slider properties

--- front/src/components/SliderMain/SliderMain.tsx
+++ front/src/components/SliderMain/SliderMain.tsx
@@ -1,7 +1,8 @@
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay, EffectFade, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 
 interface Props {
 	slides: string[];
@@ -11,12 +12,14 @@ export default function SliderMain({ slides }: Props) {
 	return (
 		<section className="section slider">
 			<Swiper
-				modules={ [Autoplay, Pagination] }
+				modules={ [Autoplay, EffectFade, Pagination] }
 				speed={ 1000 }
 				autoplay={{ delay: 3000 }}
 				pagination={{ clickable: true }}
 				slidesPerView={ 1 }
 				spaceBetween={ 0 }
+				loop={ true }
+				effect="fade"
 			>
 				{ slides.map(slide => <SwiperSlide><img className="slider__img" src={ slide }></img></SwiperSlide>) }
 			</Swiper>
