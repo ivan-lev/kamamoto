75c723372856f42c432365d172f60aa0ceb017b1
Ivan Lev <fanatos@mail.ru>
2025-09-05 15:26:41 +0500

    refactor(SliderMain): update main page slider settings

--- front/src/components/SliderMain/SliderMain.tsx
+++ front/src/components/SliderMain/SliderMain.tsx
@@ -1,7 +1,7 @@
-import { Autoplay, EffectFade, Pagination } from 'swiper/modules';
+import { Autoplay, EffectFade, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 
 interface Props {
@@ -12,16 +12,21 @@ export default function SliderMain({ slides }: Props) {
 	return (
 		<section className="section slider">
 			<Swiper
-				modules={ [Autoplay, EffectFade, Pagination] }
+				modules={ [Autoplay, EffectFade, Navigation] }
+				navigation={ true }
 				speed={ 1000 }
 				autoplay={{ delay: 3000 }}
-				pagination={{ clickable: true }}
 				slidesPerView={ 1 }
 				spaceBetween={ 0 }
 				loop={ true }
 				effect="fade"
 			>
-				{ slides.map(slide => <SwiperSlide><img className="slider__img" src={ slide }></img></SwiperSlide>) }
+				{ slides.map((slide, i) => (
+					<SwiperSlide key={ i }>
+						<img className="slider__img" src={ slide }></img>
+					</SwiperSlide>
+				),
+				) }
 			</Swiper>
 		</section>
 	);
