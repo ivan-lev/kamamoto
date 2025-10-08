fb246339b983f01bd740c90c65a0848c4ae4b9da
Ivan Lev <fanatos@mail.ru>
2025-10-08 21:25:24 +0500

    feat(Seo): add title and description to 404 page

--- front/src/components/NotFound/NotFound.tsx
+++ front/src/components/NotFound/NotFound.tsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
+import Seo from '@/components/Seo/Seo';
 import './NotFound.scss';
 
 export default function NotFound() {
 	return (
-		<section className="section">
-			<div className="not-found">
-				<span className="not-found__text">Страница не найдена 👺</span>
-				<Link to="/" className="not-found__back-link">На главную</Link>
-				<a className="link link--muted not-found__image-source-link" href="https://www.tokoname-kankou.net/en/spot/detail/9/">Источник фото</a>
-			</div>
-		</section>
+		<>
+			<Seo title="Камамото: страница не найдена" />
+			<section className="section">
+				<div className="not-found">
+					<span className="not-found__text">Страница не найдена 👺</span>
+					<Link to="/" className="not-found__back-link">На главную</Link>
+					<a className="link link--muted not-found__image-source-link" href="https://www.tokoname-kankou.net/en/spot/detail/9/">Источник фото</a>
+				</div>
+			</section>
+		</>
 	);
 }
