f4ac8b8cfd5d45ebd6aab4e0c678fb65a2977b92
Ivan Lev <fanatos@mail.ru>
2025-10-31 09:14:15 +0500

    fix(animation): revert transitions to default

--- front/src/components/Main/Main.tsx
+++ front/src/components/Main/Main.tsx
@@ -1,29 +1,12 @@
-import { motion } from 'motion/react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './Main.scss';
 
 export default function Main() {
-	const pathname = useLocation().pathname;
-
-	const pageVariants = {
-		initial: { opacity: 0 },
-		animate: { opacity: 1 },
-	};
-
-	const pageTransition = {
-		duration: 0.8,
-	};
-
 	return (
-		<motion.main
+		<main
 			className="content"
-			key={ pathname }
-			initial="initial"
-			animate="animate"
-			variants={ pageVariants }
-			transition={ pageTransition }
 		>
 			<Outlet />
-		</motion.main>
+		</main>
 	);
 };
