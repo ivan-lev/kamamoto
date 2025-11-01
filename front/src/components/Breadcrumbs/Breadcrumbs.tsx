8d22a49c912503e2d1535983b18b57f7a2694548
Ivan Lev <fanatos@mail.ru>
2025-11-01 15:47:27 +0500

    feat(breadcrumbs): add condition for button text rendering

--- front/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ front/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -2,13 +2,14 @@ import { Link, useLocation } from 'react-router-dom';
 import './Breadcrumbs.scss';
 
 export default function Breadcrumbs() {
-	const location = useLocation();
+	const location = useLocation().pathname;
+	const topLevelPaths = ['/collection/', '/exhibitions/', '/useful/'];
 
 	return (
 		<div className="breadcrumbs">
 			<Link to="../" className="link link_navigational" relative="path">
 				<img className="link__icon" src="/__spritemap#sprite-arrow-left-view" alt="иконка" />
-				{ location.pathname === '/collection/' ? 'На главную' : 'Назад' }
+				{ topLevelPaths.includes(location) ? 'На главную' : 'Назад' }
 			</Link>
 		</div>
 	);
