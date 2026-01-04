e674a993332fe34288ef8b34490886db2939cc71
Ivan Lev <fanatos@mail.ru>
2026-01-04 23:41:51 +0500

    feat(logo): add logo size attributes

--- front/src/components/visitor/Logo/Logo.tsx
+++ front/src/components/visitor/Logo/Logo.tsx
@@ -4,7 +4,7 @@ import './Logo.scss';
 export default function Logo() {
 	return (
 		<NavLink className="logo" to="/">
-			<img className="logo__img" src="/logo.svg" alt="Логотип" />
+			<img className="logo__img" src="/logo.svg" alt="Логотип" width="146" height="20" />
 		</NavLink>
 	);
 }
