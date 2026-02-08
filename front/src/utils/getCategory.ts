68750523f2ce99c5a9062eec102f6019f1307668
Ivan Lev <fanatos@mail.ru>
2026-02-08 11:35:53 +0500

    refactor(): delete unused utils

--- front/src/utils/getCategory.ts
+++
@@ -1,6 +0,0 @@
-export function getCategory(location: string): string {
-	const parsedLocation = location.split('/');
-	const indexOfCollection = parsedLocation.indexOf('collection');
-	const category = parsedLocation[indexOfCollection + 1];
-	return category;
-}
