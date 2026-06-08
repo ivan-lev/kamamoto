2356967672041cd11bed897d9604a2f4d36768f7
Ivan Lev <fanatos@mail.ru>
2026-06-08 21:45:06 +0500

    feat(dictionary): add new definitions

--- front/src/variables/useful/dictionary/b.ts
+++ front/src/variables/useful/dictionary/b.ts
@@ -3,7 +3,7 @@ import type { DictionarySection } from '@/variables/useful/dictionary/dictionary
 export const b: DictionarySection = {
 	letter: 'б',
 	terms: [
-		{ title: '<span id="bidoro">Бидоро</span>', kanji: 'ビードロ', definition: 'дословно "яблоко" - форма керамической чайницы тяирэ' },
+		{ title: '<span id="bidoro">Бидоро</span>', kanji: 'ビードロ', definition: 'термин, произошедший от португальское "стекло", в гончарном деле относится к типу природной глазури, которая главным образом образуется, когда зола от сгорающих при обжиге дров плавится на поверхности изделий и превращается в стекловидное вещество' },
 		{ title: '<span id="bunrin">Бунрин</span>', kanji: '文琳', definition: 'дословно "яблоко" - форма керамической чайницы тяирэ' },
 	],
 };
