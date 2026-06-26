52ef57a3f28f5de2f554ff111c6af14f0863b1de
Ivan Lev <fanatos@mail.ru>
2026-06-26 14:23:21 +0500

    feat(dictionary): add romaji and images

--- front/src/variables/useful/dictionary/b.ts
+++ front/src/variables/useful/dictionary/b.ts
@@ -3,8 +3,8 @@ import type { DictionarySection } from '@/variables/useful/dictionary/dictionary
 export const b: DictionarySection = {
 	letter: 'б',
 	terms: [
-		{ title: 'Бидоро', id: 'bidoro', kanji: 'ビードロ', image: 'bidoro.webp', definition: 'термин, произошедший от португальское "стекло", в гончарном деле относится к типу природной глазури, которая главным образом образуется, когда зола от сгорающих при обжиге дров плавится на поверхности изделий и превращается в стекловидное вещество' },
+		{ title: 'Бидоро', id: 'bidoro', kanji: 'ビードロ', romaji: 'bidoro', image: 'bidoro.webp', definition: 'термин, произошедший от португальское "стекло", в гончарном деле относится к типу природной глазури, которая главным образом образуется, когда зола от сгорающих при обжиге дров плавится на поверхности изделий и превращается в стекловидное вещество' },
 
-		{ title: 'Бунрин', id: 'bunrin', kanji: '文琳', image: 'bunrin.avif', definition: 'дословно "яблоко" - форма керамической чайницы тяирэ' },
+		{ title: 'Бунрин', id: 'bunrin', kanji: '文琳', romaji: 'bunrin', image: 'bunrin.avif', definition: 'дословно "яблоко" - форма керамической чайницы тяирэ' },
 	],
 };
--- front/src/variables/useful/dictionary/yo.ts
+++ front/src/variables/useful/dictionary/yo.ts
@@ -3,8 +3,8 @@ import type { DictionarySection } from '@/variables/useful/dictionary/dictionary
 export const yo: DictionarySection = {
 	letter: 'ё',
 	terms: [
-		{ title: 'Ёбицуги', id: 'yobitsugi', kanji: '呼び継ぎ', definition: 'теника восстановления (ремонта) предметов, при котором утраты замещаются частями других изделий,' },
+		{ title: 'Ёбицуги', id: 'yobitsugi', kanji: '呼継 или 呼び継ぎ', romaji: 'yobitsugi youbitsugi', image: 'yobitsugi.jpg', definition: 'теника восстановления (ремонта) предметов, при котором утраты замещаются частями других изделий,' },
 
-		{ title: 'Ёхэн', id: 'yohen', kanji: '窯変', definition: 'изменения, произошедшие с предметом во время обжига в печи - потёки, наплывы, образование зольной глазури, также деформация предмета и т.д.' },
+		{ title: 'Ёхэн', id: 'yohen', kanji: '窯変', romaji: 'yohen youhen', image: 'yohen.jpg', definition: 'изменения, произошедшие с предметом во время обжига в печи - потёки, наплывы, образование зольной глазури, также деформация предмета и т.д.' },
 	],
 };
