46248c9c7f0fd515d0b2c3128135393cf641c081
Ivan Lev <fanatos@mail.ru>
2026-07-26 01:15:18 +0500

    refactor(NotFound): change title font size

--- front/src/components/visitor/NotFound/NotFound.tsx
+++ front/src/components/visitor/NotFound/NotFound.tsx
@@ -13,7 +13,7 @@ export default function NotFound() {
 			<Header />
 			<section className="section">
 				<div className="not-found">
-					<span className="not-found__text">Страница не найдена 👺</span>
+					<span className="not-found__text title title--1">Страница не найдена 👺</span>
 					<Link to="/" className="not-found__back-link">На главную</Link>
 					<a className="link link--muted not-found__image-source-link" href="https://www.tokoname-kankou.net/en/spot/detail/9/">Источник фото</a>
 				</div>
