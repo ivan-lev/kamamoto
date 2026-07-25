f39abdcb2d409d1af69e784a6801cc2f6dd99e4b
Ivan Lev <fanatos@mail.ru>
2026-07-26 00:48:15 +0500

    fix(scroll): handle scroll to top and scroll to hash fighting

--- front/src/utils/scrollToTop.ts
+++ front/src/utils/scrollToTop.ts
@@ -1,4 +1,8 @@
 export function scrollToTop() {
+	// don't fight ScrollToHash: if the URL has an anchor, let it own the scroll position
+	if (window.location.hash)
+		return;
+
 	window.scrollTo({
 		top: 0,
 		left: 0,
